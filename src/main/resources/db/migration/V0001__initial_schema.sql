c8380f3b5dc8107dc2cb75a661cf2053ea6755c1
Diego Silva <diego.alveslv@gmail.com>
2024-07-03 18:42:04 -0300

    add initial schema migration

---
+++ src/main/resources/db/migration/V0001__initial_schema.sql
@@ -0,0 +1,64 @@
+create table user_account
+(
+    id                      serial primary key,
+    profile_name            varchar(60)              not null,
+    profile_avatar_metadata json,
+    username                varchar(40)              not null,
+    email                   varchar(255)             not null,
+    password                text                     not null,
+    created_at              timestamp with time zone not null,
+    constraint username_uq unique (username),
+    constraint email_uq unique (email),
+    constraint profile_name_uq unique (profile_name)
+);
+
+create table user_follows
+(
+    id              serial primary key,
+    user_id         integer                  not null,
+    follows_user_id integer                  not null,
+    followed_at     timestamp with time zone not null,
+    constraint user_id_fk foreign key (user_id) references user_account (id),
+    constraint follows_user_id_fk foreign key (follows_user_id) references user_account (id),
+    constraint user_id_follows_user_id_uq unique (user_id, follows_user_id)
+);
+
+create table post
+(
+    id                 serial primary key,
+    user_id            integer                  not null,
+    comment_to_post_id integer,
+    content            varchar(500),
+    created_at         timestamp with time zone not null,
+    media_metadata     json,
+    constraint user_id_fk foreign key (user_id) references user_account (id),
+    constraint comment_to_post_id_fk foreign key (comment_to_post_id) references post (id)
+);
+
+create index post_comment_to_post_id_idx on post (comment_to_post_id);
+
+create table repost
+(
+    id                  serial primary key,
+    post_id             integer                  not null,
+    reposted_by_user_id integer                  not null,
+    reposted_at         timestamp with time zone not null,
+    constraint post_id_fk foreign key (post_id) references post (id),
+    constraint reposted_by_user_id_fk foreign key (reposted_by_user_id) references user_account (id),
+    constraint post_id_reposted_by_user_id_uq unique (post_id, reposted_by_user_id)
+);
+
+create index repost_post_id_idx on repost (post_id);
+
+create table post_like
+(
+    id               serial primary key,
+    post_id          integer                  not null,
+    liked_by_user_id integer                  not null,
+    liked_at         timestamp with time zone not null,
+    constraint post_id_fk foreign key (post_id) references post (id),
+    constraint liked_by_user_id_fk foreign key (liked_by_user_id) references user_account (id),
+    constraint post_id_liked_by_user_id_uq unique (post_id, liked_by_user_id)
+);
+
+create index post_like_post_id_idx on post_like (post_id);
